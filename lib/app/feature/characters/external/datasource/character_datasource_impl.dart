3b5aa4c85ea26b6420cf6fe87b57b3f41a64253e
Emanoel S. Vieira <dev.emanoel@gmail.com>
2022-10-01 18:11:27 -0300

    feat: add external layer

---
+++ lib/app/feature/characters/external/datasource/character_datasource_impl.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+
+import '../../infra/datasource/character_datasource.dart';
+import '../../infra/model/character_model.dart';
+
+class CharacterDatasourceImpl implements CharacterDatasource {
+  final Dio _dio;
+
+  CharacterDatasourceImpl(this._dio);
+
+  @override
+  Future<List<CharacterModel>> listCharacters(int page) => _dio
+      .get('/character/?page=$page')
+      .then((response) => response.data['results'])
+      .then((results) =>
+          results.map<CharacterModel>(CharacterModel.fromJson).toList());
+}
