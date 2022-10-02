09d30dc8d5662660270eb8ad7a66be553cd2913d
Emanoel S. Vieira <dev.emanoel@gmail.com>
2022-10-02 11:00:15 -0300

    refactor: optimize dependecy injection

--- lib/app/app_page.dart
+++ lib/app/app_page.dart
@@ -1,11 +1,6 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-
-import 'feature/characters/domain/use_case/character_usecase.dart';
-import 'feature/characters/external/datasource/character_datasource_impl.dart';
-import 'feature/characters/infra/repository/character_repository_impl.dart';
+import 'di/dependency.dart';
 import 'feature/characters/presenter/characters_page.dart';
-import 'feature/characters/presenter/characters_store.dart';
 
 class AppPage extends StatelessWidget {
   const AppPage({super.key});
@@ -14,9 +9,6 @@ class AppPage extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
         ),
-        home: CharactersPage(CharactersStore(CharacterUseCaseImpl(
-            CharacterRepositoryImpl(CharacterDatasourceImpl(Dio(
-          BaseOptions(baseUrl: 'https://rickandmortyapi.com/api/'),
-        )))))),
+        home: CharactersPage(Dependency.charactersStore),
       );
 }
